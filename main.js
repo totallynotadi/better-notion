c3a459c2ea48fd5dfb6b4f03899e8cec67680904
addyett <g.aditya2048@gmail.com>
2023-06-27 00:57:44 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const { app, BrowserWindow } = require("electron");
+const {
+    setupTitlebar,
+    attachTitlebarToWindow,
+} = require("custom-electron-titlebar/main");
+
+const path = require("path");
+
+// setupTitlebar();
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1400,
+        height: 800,
+        title: "Notion",
+        titleBarOverlay: {
+            color: "#191919",
+            // color: "ffffff",
+            symbolColor: "#ffffff",
+            height: 46,
+        },
+        titleBarStyle: "hidden",
+        backgroundColor: "#191919",
+        webPreferences: {
+            sandbox: false,
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    mainWindow.loadURL("https://notion.so");
+
+    // attachTitlebarToWindow(mainWindow);
+    return mainWindow;
+};
+
+app.whenReady().then(() => {
+    let mainWindow = createWindow();
+
+    let contents = mainWindow.webContents;
+
+    contents.insertCSS(
+        `
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap');
+
+        .notion-app {
+            font-family: 'Inter', Helvetica, apple-system, sans-serif;
+        }
+
+        .notion-sidebar-container {
+            padding-top: 2rem !important;
+        }
+        .notion-topbar-action-buttons {
+            margin-right: 9rem;
+        }
+        `
+    );
+});
+
+// .notion-topbar {
+//     margin-top: 2rem !important;
+// }
